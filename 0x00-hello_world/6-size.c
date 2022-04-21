9752cc347f7dbe30c2e1fcf4ec06fb916c333718
lartehpaul <lartehapaul@gmail.com>
2022-04-21 15:24:58 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,3 +10,5 @@ int main(void)
 	printf("size of an long int: %i byte(s)\n", sizeof(long int));
 	printf("size of an long long int: %i byte(s)\n", sizeof(long long int));
 	printf("size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
