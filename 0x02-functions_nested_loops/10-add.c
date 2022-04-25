a832d5a96505e8ee8e51883542e9884df6dac1f3
lartehpaul <lartehpaul@gmail.com>
2022-04-24 17:58:24 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+171 Bytes
+#include "main.h"
+/**
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
+ */
+
+
+int add(int a, int b)
+{
+return (a + b);
+}
