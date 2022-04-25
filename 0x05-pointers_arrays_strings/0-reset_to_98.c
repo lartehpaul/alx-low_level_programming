1cb373437ecf6a991e88edc194782c6f3fffaab7
lartehpaul <lartehpaul@gmail.com>
2022-04-25 04:04:43 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
-void reset_to_98(int *);
-
+#include "main.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+* reset_to_98 - updates the value it points
+*@n: pointer of the direcction at the variable n
+*Return: 0 (success)
+*/
+void reset_to_98(int *n)
 {
-	int n;
-
-	n = 402;
-	printf("%d\n", n);
-	reset_to_98(&n);
-	printf("%d\n", n);
-	return (0);
+	*n = 98;
 }
